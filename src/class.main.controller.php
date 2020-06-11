d7ff8a505d6ddbd81c12db8d3c1458e3693e77f3
Ian Tumulak <edden87@gmail.com>
2020-06-12 01:21:28 +0800

    Added sign-in handling

--- src/class.main.controller.php
+++ src/class.main.controller.php
@@ -18,8 +18,54 @@ class WP_Firebase_Main extends WP_Firebase {
 
 		/** Main */
 		wp_enqueue_script( self::JS_MAIN, plugin_dir_url( __DIR__) . 'js/main.js', ['jquery', self::JS_FIREBASE_AUTH], '', 'true' );
-		wp_localize_script( self::JS_MAIN, 'wp_firebase', stripslashes( WP_Firebase_Admin::get_config() ));
+		wp_localize_script( self::JS_MAIN, 'wp_firebase', WP_Firebase_Admin::get_config() );
 		/**  */
+	}
+
+	public function handle_verification( $response ) {
+		$response = json_decode( $response, true );
+		$message = '';
+
+		if ( $response['code'] ) {
+			switch ( $response['code'] ) {
+				case 'auth/user-disabled':
+				case 'auth/user-not-found':
+				case 'auth/wrong-password':
+				case 'auth/invalid-email':
+					$message = $response['message'];
+				default:
+					$message = 'An error has occured. Please contact admin.';
+			}
+		}
+		else if ( $response['operationType'] ) {
+			switch ( $response['operationType'] ) {
+				case 'signIn':
+					$this->email_pass_auth( $response['user'] );
+			}
+		}
+	}
+
+	public function verify_user( $email ) {
+		return email_exists( $email );
+	}
+
+	private function email_pass_auth( $user ) {
+		if ( $this->verify_user( $user['email'] ) )
+		{
+			// Proceed logging in...
+		}
+		else {
+			// Login exist in Firebase but no wp credentials
+			// Let's create a new user
+		}
+	}
+
+	private function google_auth( $response ) {
+
+	}
+
+	private function facebook_auth( $response ) {
+
 	}
 }
 

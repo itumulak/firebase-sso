003a63a53c0b5abf92811ea162a090028ad042d7
Ian Tumulak <edden87@gmail.com>
2022-02-24 19:26:49 +0800

    Make google auth a modular component.

--- src/js/auth/google.auth.js
+++ src/js/auth/google.auth.js
@@ -1,55 +1,41 @@
 import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 import $ from 'jquery';
-import { initializeApp } from 'firebase/app';
-
-const auth = getAuth();
-const provider = new GoogleAuthProvider();
 
 const googleAuth = () => {
-	$.post(
-		// eslint-disable-next-line no-undef
-		firebase_ajaxurl,
-		{
-			action: 'firebase_config',
-		},
-		(response) => {
-			if (response.success === true) {
-				initializeApp(response.data.config);
+	const auth = getAuth();
+	const provider = new GoogleAuthProvider();
 
-				signInWithPopup(auth, provider)
-					.then((result) => {
-						const token = result.credential.accessToken;
-						const user = result.user;
+	signInWithPopup(auth, provider)
+		.then((result) => {
+			const token = result.credential.accessToken;
+			const user = result.user;
 
-						$.post(
-							// eslint-disable-next-line no-undef
-							firebase_ajaxurl,
-							{
-								action: 'firebase_google_login',
-								oauth_token: token,
-								refresh_token: user.refreshToken,
-								email: user.email,
-							},
-							(e) => {
-								if (e.success === true) {
-									window.location.href = e.data.url;
-								}
-							}
-						);
-					})
-					.catch((error) => {
-						// Handle Errors here.
-						const errorCode = error.code;
-						const errorMessage = error.message;
-						// The email of the user's account used.
-						const email = error.email;
-						// The AuthCredential type that was used.
-						const credential = GoogleAuthProvider.credentialFromError(error);
-						// ...
-					});
-			}
-		}
-	);
+			$.post(
+				// eslint-disable-next-line no-undef
+				firebase_ajaxurl,
+				{
+					action: 'firebase_google_login',
+					oauth_token: token,
+					refresh_token: user.refreshToken,
+					email: user.email,
+				},
+				(e) => {
+					if (e.success === true) {
+						window.location.href = e.data.url;
+					}
+				}
+			);
+		})
+		.catch((error) => {
+			// Handle Errors here.
+			const errorCode = error.code;
+			const errorMessage = error.message;
+			// The email of the user's account used.
+			const email = error.email;
+			// The AuthCredential type that was used.
+			const credential = GoogleAuthProvider.credentialFromError(error);
+			// ...
+		});
 };
 
 export default googleAuth;
--- src/js/base.auth.js
+++ src/js/base.auth.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import $ from 'jquery';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import googleAuth from './auth/google.auth';
 
 let firebaseConfig;
 
@@ -21,47 +21,6 @@ $(document).ready(() => {
 	);
 
 	$('#wp-firebase-google-sign-in').on('click', () => {
-		console.log('Google Auth triggered...');
-
-		/**
-		 * @todo put this on a separate file so that it can be modular.
-		 *
-		 */
-		const auth = getAuth();
-		const provider = new GoogleAuthProvider();
-
-		signInWithPopup(auth, provider)
-			.then((result) => {
-				const token = result.credential.accessToken;
-				const user = result.user;
-
-				$.post(
-					// eslint-disable-next-line no-undef
-					firebase_ajaxurl,
-					{
-						action: 'firebase_google_login',
-						oauth_token: token,
-						refresh_token: user.refreshToken,
-						email: user.email,
-					},
-					(e) => {
-						if (e.success === true) {
-							window.location.href = e.data.url;
-						}
-					}
-				);
-			})
-			.catch((error) => {
-				// Handle Errors here.
-				const errorCode = error.code;
-				const errorMessage = error.message;
-				// The email of the user's account used.
-				const email = error.email;
-				// The AuthCredential type that was used.
-				const credential =
-					GoogleAuthProvider.credentialFromError(error);
-				// ...
-			});
-		/** end */
+		googleAuth();
 	});
 });

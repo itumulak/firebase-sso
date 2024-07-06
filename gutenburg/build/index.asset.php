2ff49cf1e7a09b072c0342f2b6f45381df54eb0a
Ian Tumulak <edden87@gmail.com>
2024-07-06 18:55:21 +0800

    Remove uncessary files and code clean up.

--- gutenburg/build/index.asset.php
+++ gutenburg/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'bfea54092584bcb6820a');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'a359bb4637f9454f4a16');
--- gutenburg/src/edit.js
+++ gutenburg/src/edit.js
@@ -1,26 +1,7 @@
-/**
- * Retrieves the translation of text.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
- */
 import { __ } from '@wordpress/i18n';
-
-/**
- * React hook that is used to mark the block wrapper element.
- * It provides all the necessary props like the class name.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
- */
 import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
 import { PanelBody, ToggleControl } from "@wordpress/components";
 
-/**
- * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
- * Those files can contain any CSS code that gets applied to the editor.
- *
- * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
- */
-import './editor.scss';
 import '../../src/View/Frontend/assets/styles/login.css';
 import facebookIconSvg from "../../src/assets/images/facebook-logo.svg";
 import googleIconSvg from "../../src/assets/images/google-logo.svg";
